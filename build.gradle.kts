6564b42c6c27c424c300cc5d7386e0e86fd95cb8
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2026-09-04 13:45:46 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    id("org.jetbrains.intellij.platform")
+    id("org.jetbrains.changelog")
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+
+    // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
+    intellijPlatform {
+        intellijIdea("2025.2.6.2")
+        testFramework(TestFrameworkType.Platform)
+    }
+}
